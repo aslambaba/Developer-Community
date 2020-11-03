736d50a23b7e93ceba4c9ac1b6b6a7f8c06e1a28
aslambaba <42738148+aslambaba@users.noreply.github.com>
2020-11-03 12:18:22 +0500

    Setup Contentful Using dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
 module.exports = {
-    plugins: [`gatsby-plugin-material-ui`],
+    plugins: [
+      `gatsby-plugin-material-ui`,
+      {
+        resolve: `gatsby-source-contentful`,
+        options: {
+          spaceId: process.env.DC_SPACEID,
+          accessToken: process.env.DC_TOKEN,
+        },
+      },
+    ],
   };
