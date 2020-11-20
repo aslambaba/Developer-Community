d6b32c2905679b892882110e5dae07d369430f40
Aslam Sarfraz <aslam91r@gmail.com>
2020-11-20 08:13:58 +0500

    Complete Developer Community Blog - Update STyling

--- src/templates/developers.js
+++ src/templates/developers.js
@@ -8,11 +8,11 @@ function Developers({ pageContext }) {
     return (
         <Layout>
             <div className={Style.MainConatiner}>
-                <Grid container spacing={5}>
+                <Grid container spacing={5} className={Style.GridCon}>
                     {
                         Object.keys(pageContext).map(obj => {
                             return (
-                                <Grid item xs={4} className={Style.Grid} key={obj}>
+                                <Grid item className={Style.Grid} key={obj}>
                                     <img src={pageContext[obj].image.file.url} alt={pageContext[obj].name} />
                                     <h2>{pageContext[obj].name}</h2>
                                     <p>{pageContext[obj].job}</p>
