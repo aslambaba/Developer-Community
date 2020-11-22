9c01d80d512bc2aa373aa13b61c7d1329ce2875e
Aslam Sarfraz <aslam91r@gmail.com>
2020-11-22 16:23:10 +0500

    Add Firebase Authentication

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,29 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-    plugins: [
-      `gatsby-plugin-material-ui`,
-      {
-        resolve: `gatsby-source-contentful`,
-        options: {
-          spaceId: process.env.DC_SPACEID,
-          accessToken: process.env.DC_TOKEN,
+  plugins: [
+    
+    `gatsby-plugin-material-ui`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.DC_SPACEID,
+        accessToken: process.env.DC_TOKEN,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyByNw_4hUdfY5KgVygJGsYs4xrldXhyBtg",
+          authDomain: "developer-community-94e05.firebaseapp.com",
+          databaseURL: "https://developer-community-94e05.firebaseio.com>",
+          projectId: "developer-community-94e05",
+          storageBucket: "developer-community-94e05.appspot.com",
+          messagingSenderId: "166212288546",
+          appId: "1:166212288546:web:1723244da94c715a5a4a7b"
         },
       },
-    ],
-  };
+    }
+  ],
+};
--- src/templates/developer.js
+++ src/templates/developer.js
@@ -1,36 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Style from './style/developer.module.css';
 import HireMeButton from '../components/hiremeButton';
 import SocialMediaIcon from '../components/socialMedia';
 import Layout from '../components/layout';
+import firebase from "gatsby-plugin-firebase"
 
-function developer({ pageContext }) {
+
+function Developer({ pageContext }) {
+
+    const onSignupClick = () => {
+        let provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            console.log('Google Successful')
+        }).catch(function (error) {
+            console.log('Google Error')
+        });
+    }
+
+    const SignOut = () => {
+        firebase.auth().signOut().then(function() {
+            console.log('SignOut Successful !!')
+          }).catch(function(error) {
+            console.log(error);
+          });
+    }
     console.log(pageContext);
+
+    const [UserActivity, SetuserActivity] = useState();
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            SetuserActivity(true);
+        } else {
+            SetuserActivity(false)
+            // No user is signed in.
+        }
+    });
+
     return (
         <Layout>
-        <div className={Style.mainSec}>
-            <div className={Style.DeveloperSec}>
-                <div className={Style.DeveloperImage}>
-                    <img src={pageContext.image.file.url} alt='Developer' />
-                </div>
-                <div className={Style.DeveloperTitle}>
-                    <h1>{pageContext.name}</h1>
-                    <p>{pageContext.job}</p>
-                </div>
-
-                <div className={Style.DeveloperData}>
-                    <div className={Style.DeveloperBio}>
-                        <p>{pageContext.bio.internal.content}</p>
+            <div className={Style.mainSec}>
+                <div className={Style.DeveloperSec}>
+                    <div className={Style.DeveloperImage}>
+                        <img src={pageContext.image.file.url} alt='Developer' />
                     </div>
-                    <div className={Style.DeveloperSocial}>
-                        <SocialMediaIcon social={pageContext}/>
-                        <HireMeButton />
+                    <div className={Style.DeveloperTitle}>
+                        <h1>{pageContext.name}</h1>
+                        <p>{pageContext.job}</p>
+                    </div>
+
+                    <div className={Style.DeveloperData}>
+                        <div className={Style.DeveloperBio}>
+                            <p>{pageContext.bio.internal.content}</p>
+                        </div>
+                        {
+                            UserActivity === false
+                                ?
+                                <div className={Style.SignupSEC}>
+                                    <p>Please Login With Google To Contact Our Freelancer</p>
+                                    <button onClick={onSignupClick} className={Style.SignBTN}>Signup</button>
+                                </div>
+                                :
+                                <div className={Style.DeveloperSocial}>
+                                    <SocialMediaIcon social={pageContext} />
+                                    <HireMeButton />
+                                    <button onClick={SignOut} className={Style.SignBTN}>Sign Out</button>
+                                </div>
+                        }
                     </div>
                 </div>
             </div>
-        </div>
         </Layout>
     )
 }
 
-export default developer;
+export default Developer;
