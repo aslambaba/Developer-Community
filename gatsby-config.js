df9e5c955853d1ce7c2b370a141e8a17ad655252
Aslam Sarfraz <aslam91r@gmail.com>
2020-12-09 00:09:23 +0500

    Set .env

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,13 +15,13 @@ module.exports = {
       resolve: "gatsby-plugin-firebase",
       options: {
         credentials: {
-          apiKey: "AIzaSyByNw_4hUdfY5KgVygJGsYs4xrldXhyBtg",
-          authDomain: "developer-community-94e05.firebaseapp.com",
-          databaseURL: "https://developer-community-94e05.firebaseio.com>",
-          projectId: "developer-community-94e05",
-          storageBucket: "developer-community-94e05.appspot.com",
-          messagingSenderId: "166212288546",
-          appId: "1:166212288546:web:1723244da94c715a5a4a7b"
+          apiKey: process.env.FB_apiKey,
+          authDomain: process.env.authDomain,
+          databaseURL: process.env.databaseURL,
+          projectId: process.env.projectId,
+          storageBucket: process.env.storageBucket,
+          messagingSenderId: process.env.messagingSenderId,
+          appId: process.env.appId
         },
         features: {
           auth: true,
